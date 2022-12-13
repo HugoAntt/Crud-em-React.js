8f3a34bd1c7b7cc0e11445527a2cb5ed40e29f12
Hugo Antunes <86171824+HugoAntt@users.noreply.github.com>
2022-12-13 03:31:07 -0300

    Tabelas

--- src/components/Funcionarios.js
+++ src/components/Funcionarios.js
@@ -1,10 +1,51 @@
-function Funcionarios() {
+import React from "react"
+import { Table } from "react-bootstrap";
 
-    return (
-        <div>
-            <p>Esta página é sobre Funcionarios</p>
-        </div>
-    )
+
+class Funcionarios extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            funcionarios: []
+        }
+
+    }
+
+    render() {
+        return (
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Opções</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Neto </td>
+                        <td>neto@empresax.com.br </td>
+                    </tr>
+                    <tr>
+                        <td>Jonh </td>
+                        <td>Jonh@travolta.com.br </td>
+                    </tr>
+                    <tr>
+                        <td>Eduardo </td>
+                        <td>eduardinho@outlook.oi.br </td>
+                    </tr>
+                    <tr>
+                        <td>Cassandra </td>
+                        <td>odissey@gmail.com </td>
+                    </tr>
+                </tbody>
+            </Table>
+        )
+
+    }
+
 }
 
 export default Funcionarios;
